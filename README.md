e099d37a9070390d7d1faede2936294f65b0c775
fred3ric <fredreinier@gmail.com>
2020-12-13 12:26:22 +0100

    add lambda python script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AWSmisc
 My journey on AWS dev tools
+
+This repo contains my python script to interact with AWS 
--- test.py
+++ test.py
@@ -1,3 +1,20 @@
 mymessage = "hello world"
 print(mymessage)
 mymessage.capitalize()
+
+
+lucky_numbers=[4,8,34,6,343,665]
+friends=["Gilles","Michi","Eric"]
+friends.extend(lucky_numbers)
+print(friends)
+
+friends.insert(1, "kelly")
+friends.remove('Eric')
+
+print(friends)
+
+def cube(num):
+    return pow(num,3)
+
+result = cube(2)
+print(result)
